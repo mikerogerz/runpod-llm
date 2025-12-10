92b89a34c238aca33ddfc140db08600efedce48a
Mike Rogers <mikerogerz@gmail.com>
2025-12-10 16:09:33 -0500

    Fix pooling param.

--- 2_runtime_setup_llamacpp.sh
+++ 2_runtime_setup_llamacpp.sh
@@ -9,7 +9,7 @@ echo "Starting llama.cpp server..."
 
 # Set the model path and alias
 MODEL_PATH="${RUNPOD_LLM_MODEL_DIR}/${RUNPOD_LLM_MODEL_FILE_NAME}"
-ARGS="--model ${MODEL_PATH} --model_alias ${RUNPOD_LLM_MODEL_ALIAS} --embedding 1 --pooling last"
+ARGS="--model ${MODEL_PATH} --model_alias ${RUNPOD_LLM_MODEL_ALIAS} --embedding 1 --pooling 3"
 
 # Offload all layers to GPU where possible
 ARGS="${ARGS} --n_gpu_layers -1"
